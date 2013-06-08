945573374e59fb255fd9b29cc74d113144cdebf9
Sewar <xsewarx@gmail.com>
2013-06-08 18:10:10 -0500

    Change database backend to MySQL for easier debugging, settings should be stored in mysql.cnf

--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
 # Django settings for edid_db_website project.
+import os
+
+#Stolen shamelessly from http://deanproxy.com/blog/posts/2011/10/5-django-environmental-based-settings.html
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,13 +15,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test.sqlite3',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'OPTIONS': {
+            'read_default_file': os.path.join(SITE_ROOT, 'mysql.cnf'),
+        },
     }
 }
 
