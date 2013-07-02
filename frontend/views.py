748b9bc8cdd0b5a6cb787aedbd0ae5b050fb370b
Sewar <xsewarx@gmail.com>
2013-07-01 20:08:18 -0500

    Add TimingMixin to provide common functions to timing editing views.

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 
@@ -43,39 +44,76 @@ class EDIDUpdate(UpdateView):
     form_class = EDIDUpdateForm
 
 
-### Standard Timing
-class StandardTimingCreate(CreateView):
-    model = StandardTiming
-    form_class = StandardTimingForm
+### Timing Mixin
+class TimingMixin(object):
+    context_object_name = 'timing'
 
-class StandardTimingUpdate(UpdateView):
-    model = StandardTiming
-    form_class = StandardTimingForm
+    def get_object(self, queryset=None):
+        """
+        Gets timing object based on edid_pk and identification.
+
+        Used for UpdateView and DeleteView.
+        """
+
+        queryset = self.get_queryset()
+
+        edid_pk = self.kwargs.get('edid_pk', None)
+        identification = self.kwargs.get('identification', None)
+
+        obj = get_object_or_404(queryset, EDID=edid_pk, identification=identification)
+
+        return obj
 
     def get_success_url(self):
-        # Returns url of EDID updating page
-        return reverse('edid-update', kwargs={'pk': self.object.EDID.pk})
+        """
+        Returns url of EDID updating page.
 
-class StandardTimingDelete(DeleteView):
+        Used for CreateView, UpdateView and DeleteView.
+        """
+
+        edid_pk = self.kwargs.get('edid_pk')
+
+        return reverse('edid-update', kwargs={'pk': edid_pk})
+
+    def form_valid(self, form):
+        """
+        Sets EDID and identification when creating new timing.
+
+        Used for CreateView and UpdateView.
+        """
+
+        if not form.instance.EDID_id:
+            form.instance.EDID = form.EDID
+            if not form.instance.identification:
+                # Get count of available timings
+                count = self.model.objects.filter(EDID=form.instance.EDID).count()
+                # Set identification to count + 1
+                form.instance.identification = count + 1
+
+        return super(TimingMixin, self).form_valid(form)
+
+
+### Standard Timing
+class StandardTimingCreate(TimingMixin, CreateView):
     model = StandardTiming
     form_class = StandardTimingForm
-#    success_url = reverse_lazy('edid-update')
+
+class StandardTimingUpdate(TimingMixin, UpdateView):
+    model = StandardTiming
+    form_class = StandardTimingForm
+
+class StandardTimingDelete(TimingMixin, DeleteView):
+    model = StandardTiming
 
 
 ### Detailed Timing
-class DetailedTimingCreate(CreateView):
+class DetailedTimingCreate(TimingMixin, CreateView):
     model = DetailedTiming
     form_class = DetailedTimingForm
 
-class DetailedTimingUpdate(UpdateView):
+class DetailedTimingUpdate(TimingMixin, UpdateView):
     model = DetailedTiming
     form_class = DetailedTimingForm
 
-    def get_success_url(self):
-        # Returns url of EDID updating page
-        return reverse('edid-update', kwargs={'pk': self.object.EDID.pk})
-
-class DetailedTimingDelete(DeleteView):
+class DetailedTimingDelete(TimingMixin, DeleteView):
     model = DetailedTiming
-    form_class = DetailedTimingForm
-#    success_url = reverse_lazy('edid-update')
