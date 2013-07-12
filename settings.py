e2eb1b0d9577ece9b2c30600a8e3e8f1d6523d84
Sewar <xsewarx@gmail.com>
2013-07-12 07:43:09 -0500

    Update settings.py to use SQLite3 by default.

--- settings.py
+++ settings.py
@@ -15,11 +15,16 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(SITE_ROOT, 'mysql.cnf'),
-        },
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2',
+                                                # 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'edid_website.sqlite3',         # Or path to database file if
+                                                # using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',             # Empty for localhost through domain sockets
+                                # or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
